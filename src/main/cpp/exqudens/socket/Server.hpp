e578029755cc452c22a6de92bdda6470a9c93c9f
exqudens <exqudens@gmail.com>
2023-07-21 23:03:08 -0700

    step 1

---
+++ src/main/cpp/exqudens/socket/Server.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "exqudens/socket/export.hpp"
+
+namespace exqudens::socket {
+
+  class Server {
+
+    private:
+
+      int p;
+
+  };
+
+}
---
+++ src/test/cpp/TestApplication.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+class TestApplication {
+
+  public:
+
+    static int run(int* argc, char** argv);
+
+}
